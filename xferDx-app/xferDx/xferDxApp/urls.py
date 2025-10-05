fcc4fc1daa0cc786b450dacbb7f226c852c0a3dd
mfmadarang <mfmadarang@up.edu.ph>
2025-10-05 21:15:01 +0800

    Add Patient Registration Module

--- xferDx-app/xferDx/xferDxApp/urls.py
+++ xferDx-app/xferDx/xferDxApp/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # URL pattern for add patient page
+    # When user goes to /add-patient/, it will call the add_patient view
+    path('add-patient/', views.add_patient, name='add_patient'),
+    
+    # If you have a home URL pattern, add it here too
     path('', views.home, name='home'),
+    
+    # Add other URL patterns here as you build more features
 ]
